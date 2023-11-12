345ab841a81a46a9ab79650aa69d4de29145f002
Beni Fissha <benifissha@gmail.com>
2023-11-12 19:09:02 +0300

    updated

--- models/user.py
+++ models/user.py
@@ -9,10 +9,12 @@ class User(BaseModel):
     """
     User class
     """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
     def __init__(self, *args, **kwargs):
         """Inintalization"""
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
         super().__init__(*args, **kwargs)
